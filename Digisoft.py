b24f7573c2a125396c25d36aabf6e86a73a52441
RPSRana <30373809+RPSRana@users.noreply.github.com>
2019-01-13 16:00:34 +0530

    Update Digisoft.py

--- Digisoft.py
+++ Digisoft.py
@@ -2,18 +2,18 @@ import glob
 import os
 import pydicom
 
-#Reading dcm files using pydicom library
-#Function to return Dicom tags
-def DicomTags(path):
-    dataset = pydicom.dcmread(path)
-    Dicom_tags = list(dataset.keys())
-    Dicom_tags = str(Dicom_tags)
-    return Dicom_tags
+#Function to get dicom value list
+def dicom_value(data):
+    v=[]
+    for key in data.dir():
+        value = getattr(data, key, '')
+        v.append(value)
+    return v
 
 path= "E:/DigiSoft/"
 os.chdir(path) #the directory containing your dcm files
 for file in glob.glob("*.dcm"):
-    text = DicomTags(str(file))
-    f = open(( file.rsplit( ".", 1 )[ 0 ] ) + ".text", "w") #creates a new text file
-    f.write(text) #write to the text file
-    f.close()
+    dataset = pydicom.read_file(path+str(file))
+    with open(( file.rsplit( ".", 1 )[ 0 ] ) + ".text", mode="w") as outfile:
+        for s, l, m in zip(list(dataset.keys()), dataset.dir(), dicom_value(dataset)):
+            outfile.write("%s %s %s\n" % (s, l, m))                 
